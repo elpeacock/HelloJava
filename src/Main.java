65395a7bc2946415fe7da02ed7927449d2bb1486
Liz Peacock <liz.peacock11@gmail.com>
2016-12-15 14:12:45 -0600

    Primitive types exercise

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        //Hello World
         System.out.println("Hello World!");
+
+        //Primitive Types
+        System.out.println("Primitive Data Types in Java");
+        Types.printByte();
+        Types.printShort();
+        Types.printInteger();
+        Types.printDouble();
+        Types.printLong();
+        Types.printFloat();
+        Types.printCharacter();
+        Types.printBool();
+
+        //Scanner Class
+        System.out.println("Scanner Class Exercise");
+        Scan.measureRooms();
     }
 }
