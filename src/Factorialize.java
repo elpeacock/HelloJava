2ec1cc45d3ff767d27bd74e653277ed9e4e9d4eb
Liz Peacock <liz.peacock11@gmail.com>
2017-01-04 11:51:47 -0600

    moved main method into factorialize class

--- src/Factorialize.java
+++ src/Factorialize.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by user on 12/20/16.
  */
@@ -11,4 +13,25 @@ public class Factorialize {
         return fact;
     }
 
+    public static void main(String[] args) {
+        String response = "yes";
+        System.out.println("Let's get maths-y: factorials!");
+        do {
+            System.out.println("enter a number between 1 and 10: ");
+            Scanner sc = new Scanner (System.in);
+            int x = sc.nextInt();
+            sc.nextLine();
+
+            if ( ( x >= 1) && ( x <= 10 ) ) {
+                System.out.println( "factorial of " + x + " is " + Factorialize.regularStyle(x) );
+
+                System.out.println( "find another factorial? (yes/no): ");
+                response = sc.nextLine();
+            } else {
+                System.out.println( "that number is NOT between 1 and 10, try again!" );
+            }
+        } while (response.trim().equalsIgnoreCase("yes") );
+
+        System.out.println( "ok. peace out girl scout!" );
+    }
 }
