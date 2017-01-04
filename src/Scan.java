606d4febe704cf8b8f05001def13913522ce74b1
Liz Peacock <liz.peacock11@gmail.com>
2017-01-04 10:52:06 -0600

    moved main into scan class

--- src/Scan.java
+++ src/Scan.java
@@ -3,13 +3,14 @@
  */
 public class Scan {
 
-    public static void measureRooms() {
+    public static void main(String[] args) {
         String response = "yes";
         double length;
         double width;
         double area;
         double perimeter;
 
+        System.out.println("Scanner class exercises");
         System.out.println("Let's measure the rooms!!");
 
         do {
@@ -26,9 +27,13 @@ public class Scan {
             perimeter = 2 * length + 2 * width;
             System.out.println("The perimeter of this room is: " + perimeter);
 
-            System.out.println("Do you want to measure another room? yes/no: ");
-            response = sc.next();
+            // make sure not to skip the next line - when going from nextInt() or nextDouble() there is a new line
+            // character at the end that skips into nextLine(), so you have to discard that by calling nextLine()
+            // before pulling the actual input you want
             sc.nextLine();
+
+            System.out.println("Do you want to measure another room? yes/no: ");
+            response = sc.nextLine();
 
         } while("yes".equalsIgnoreCase(response));
 
