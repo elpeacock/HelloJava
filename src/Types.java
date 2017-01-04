93a696004001075bd4de674a64a3a65657361250
Liz Peacock <liz.peacock11@gmail.com>
2017-01-04 10:51:13 -0600

    moved main into types class

--- src/Types.java
+++ src/Types.java
@@ -2,44 +2,43 @@
  * Created by user on 12/15/16.
  */
 public class Types {
-    public static void printByte() {
+    public static void main(String[] args) {
+
+        System.out.println("Primitive Data Types in Java");
+
         byte aByte = 122;
         System.out.println("I am a byte: " + aByte);
-    }
 
-    public static void printShort() {
+
         short aShort = -12345;
         System.out.println("I am a short: " + aShort);
-    }
 
-    public static void printInteger() {
+
         int anInt = 1234567;
         System.out.println("I am an integer: " + anInt);
-    }
 
-    public static void printLong() {
+
         long aLong = 123345678l;
         System.out.println("I am a long: " + aLong);
-    }
 
-    public static void printFloat() {
+
+
         float aFloat = 3.1415f;
         System.out.println("I am a float: " + aFloat);
-    }
 
-    public static void printDouble() {
+
         double aDouble = 1.23456789;
         System.out.println("I am a double: " + aDouble);
-    }
 
-    public static void printCharacter() {
+
         char aCharacter = 'b';
         System.out.println("I am a character:" + aCharacter);
-    }
 
-    public static void printBool() {
+
         boolean aBool = true;
         System.out.println("I am a boolean: " + aBool);
+
     }
+
 
 }
