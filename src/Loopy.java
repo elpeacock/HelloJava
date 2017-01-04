10028320292a2c4d3ae6425653776f54f9ec6529
Liz Peacock <liz.peacock11@gmail.com>
2017-01-04 11:23:08 -0600

    moved main method into loopy class

--- src/Loopy.java
+++ src/Loopy.java
@@ -5,90 +5,80 @@ import java.util.Scanner;
  */
 public class Loopy {
 
-    //while loop
-    public static void whileLoop() {
+    public static void main(String[] args) {
+        //while loop
         int i = 5;
 
         while (i <= 15) {
             System.out.println("i is " + i);
             i++;
         }
-    }
 
-    //do-whiles
-    public static void evens() {
-        int i = 0;
+        //do-whiles
+        int a = 0;
 
+        System.out.println("print even numbers from 0-100");
         do {
-            i += 2;
-            System.out.println(i);
-        } while (i < 100);
-    }
+            a += 2;
+            System.out.println(a);
+        } while (a < 100);
 
-    public static void fives() {
-        int i = 100;
+        int b = 100;
 
+        System.out.println("count from 100 to -10 by fives");
         do {
-            System.out.println(i);
-            i -= 5;
-        } while (i >= -10);
-    }
+            System.out.println(b);
+            b -= 5;
+        } while (b >= -10);
 
-    public static void squares() {
         long x = 2l;
 
+        System.out.println("Print list of perfect squares less than 1000000");
         do {
             System.out.println(x);
             x *= x;
         } while (x < 1000000);
 
-    }
+        //for loops
+        int c;
 
-    //for loops
-    public static void forEvens() {
-        int i;
-
-        for (i = 0; i <= 100; i += 2) {
-            System.out.println(i);
+        System.out.println("evens from 0-100 with a for loop");
+        for (c = 0; c <= 100; c += 2) {
+            System.out.println(c);
         }
-    }
 
-    public static void forFives() {
-        int i;
+        int d;
 
-        for (i = 100; i >= -10; i -= 5) {
-            System.out.println(i);
+        System.out.println("count from 100 to -10 by fives with a for loop");
+        for (d = 100; d >= -10; d -= 5) {
+            System.out.println(d);
         }
-    }
 
-    public static void forSquares() {
-        long i;
+        long e;
 
-        for (i = 2l; i <= 1000000; i *= i) {
-            System.out.println(i);
+        System.out.println("list of perfect squares up to 1000000 with a for loop");
+        for (e = 2l; e <= 1000000; e *= e) {
+            System.out.println(e);
         }
-    }
 
-    //FizzBuzz
-    public static void fizzBuzz() {
-        int i;
+        //FizzBuzz
+        int f;
 
-        for (i = 1; i <= 100; i += 1) {
-            if ((i % 3 == 0) && (i % 5 == 0)) {
+        for (f = 1; f <= 100; f += 1) {
+            if ((f % 3 == 0) && (f % 5 == 0)) {
                 System.out.println("fizzBuzz!");
-            } else if (i % 3 == 0) {
+            } else if (f % 3 == 0) {
                 System.out.println("fizz!");
-            } else if (i % 5 == 0) {
+            } else if (f % 5 == 0) {
                 System.out.println("buzz!");
             } else {
-                System.out.println(i);
+                System.out.println(f);
             }
         }
-    }
 
-    //Powers
-    public static void showMeThePowers() {
-        int i;
+        //Powers
+
+        int g;
         int guess;
         String response = "yes";
 
@@ -99,14 +89,14 @@ public class Loopy {
             guess = sc.nextInt();
 
             System.out.println("Even Squares between 1 and " + guess + " are: ");
-            for (i = 1; i * i < guess; i++) {
-                System.out.print(i * i + " ");
+            for (g = 1; g * g < guess; g++) {
+                System.out.print(g * g + " ");
             }
             System.out.println(" ");
 
             System.out.println("Even Cubes between 1 and " + guess + " are: ");
-            for (i = 1; Math.pow(i, 3) < guess; i++) {
-                System.out.print((int) Math.pow(i, 3) + " ");
+            for (g = 1; Math.pow(g, 3) < guess; g++) {
+                System.out.print((int) Math.pow(g, 3) + " ");
             }
             System.out.println(" ");
             System.out.println("Do you want to find more squares/cubes? (yes/no): ");
@@ -115,14 +105,12 @@ public class Loopy {
         } while ("yes".equalsIgnoreCase(response));
 
         System.out.println ("See ya later, Alligator");
-    }
 
-    // Percentage to Letter Grades
-    public static void letterGrades() {
+        // Percentage to Letter Grades
         // the number percentage user gives
         int numberGrade;
         // response for do while loop
-        String response = "yes";
+        String convertAnotherGrade = "yes";
         // holds the letter grade
         String letterGrade;
 
@@ -173,14 +161,21 @@ public class Loopy {
 
             System.out.println(numberGrade + " is a(n) " + letterGrade);
             System.out.println("convert another grade? (yes/no): ");
-            response = sc.next();
+            convertAnotherGrade = sc.next();
             sc.nextLine();
 
-        } while ("yes".equalsIgnoreCase(response));
+        } while ("yes".equalsIgnoreCase(convertAnotherGrade));
 
         System.out.println("see ya later");
 
     }
+
+
+
+
+
+
+
 
 
 
