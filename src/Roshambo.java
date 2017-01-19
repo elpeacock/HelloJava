a90ec77c8a97967e85663c048a34cf50b4d7eedb
Liz Peacock <liz.peacock11@gmail.com>
2017-01-19 09:48:39 -0600

    created Rock/paper/scissors enum, also did enum comparison method

---
+++ src/Roshambo.java
@@ -0,0 +1,50 @@
+/**
+ * Created by user on 1/17/17.
+ */
+
+/**
+ * Create an enumeration called Roshambo that stores three values: rock, paper, and scissors.
+ * This enumeration should include a toString method that can convert the selected value to a String.
+ */
+public enum Roshambo {
+    ROCK, PAPER, SCISSORS;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static String compareRoshambo(String myRoshambo, String opponentRoshambo, RoshamboPlayer opponent,
+                                         RoshamboPlayer human) {
+        String message = "";
+        if (myRoshambo.equals(opponentRoshambo)){
+            message = "Tie!!";
+        } else if ( (myRoshambo.equalsIgnoreCase("ROCK") ) && (opponentRoshambo.equalsIgnoreCase("PAPER") ) ) {
+            message = "Paper covers Rock - " + opponent.getName() + " wins!";
+            opponent.gamesWon++;
+            human.gamesLost++;
+        } else if ( (myRoshambo.equalsIgnoreCase("ROCK") ) && (opponentRoshambo.equalsIgnoreCase("SCISSORS") ) ) {
+            message = "Rock smashes Scissors - " + human.getName() + " wins!";
+            human.gamesWon++;
+            opponent.gamesLost++;
+        } else if ( (myRoshambo.equalsIgnoreCase("PAPER") ) && (opponentRoshambo.equalsIgnoreCase("ROCK") ) ) {
+            message = "Paper covers Rock - " + human.getName() + " wins!";
+            human.gamesWon++;
+            opponent.gamesLost++;
+        } else if ( (myRoshambo.equalsIgnoreCase("PAPER") ) && (opponentRoshambo.equalsIgnoreCase("SCISSORS") ) ) {
+            message = "Scissors cut Paper - " + opponent.getName() + " wins!";
+            opponent.gamesWon++;
+            human.gamesLost++;
+        } else if ( (myRoshambo.equalsIgnoreCase("SCISSORS") ) && (opponentRoshambo.equalsIgnoreCase("ROCK") ) ) {
+            message = "Rock smashes Scissors - " + opponent.getName() + " wins!";
+            opponent.gamesWon++;
+            human.gamesLost++;
+        } else if ( (myRoshambo.equalsIgnoreCase("SCISSORS") ) && (opponentRoshambo.equalsIgnoreCase("PAPER") ) ) {
+            message = "Scissors cut Paper - " + human.getName() + " wins!";
+            human.gamesWon++;
+            opponent.gamesLost++;
+        }
+        return message;
+    }
+}
+
