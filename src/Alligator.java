1c46fe77f8f9aba6897ad3b63b07865155a0ba7b
Liz Peacock <liz.peacock11@gmail.com>
2017-01-20 10:59:54 -0600

    Added the Alligator class to implement countable interface Added main method in CountTest class in order to test

---
+++ src/Alligator.java
@@ -0,0 +1,26 @@
+/**
+ * Created by user on 1/20/17.
+ */
+public class Alligator implements Countable {
+    private int count = 0;
+
+    @Override
+    public void incrementCount() {
+        count++;
+    }
+
+    @Override
+    public void resetCount() {
+        count = 0;
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String getCountString() {
+        return count + " alligator";
+    }
+}
---
+++ src/CountTest.java
@@ -0,0 +1,27 @@
+/**
+ * Created by user on 1/20/17.
+ */
+public class CountTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        System.out.println("Counting alligators... ");
+        System.out.println("");
+        Countable alligator = new Alligator();
+        CountUtil.count(alligator, 3);
+
+        System.out.println("");
+        System.out.println("Counting Sheeps... ");
+        System.out.println("");
+        Sheep blackie = new Sheep("Blackie");
+        CountUtil.count(blackie, 2);
+        System.out.println("");
+
+        Sheep dolly = (Sheep) blackie.clone();
+        dolly.setName("Dolly");
+        CountUtil.count(dolly, 3);
+        System.out.println("");
+
+        blackie.resetCount();
+        CountUtil.count(blackie, 1);
+    }
+}
